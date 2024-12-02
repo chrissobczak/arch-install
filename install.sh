a06c1ec425ee490572e26baedd4d4eefa230e12c
Chris Sobczak <chris@sobczak.family>
2024-12-01 21:12:20 -0600

    Configure bootloader

--- install.sh
+++ install.sh
@@ -1,68 +1,64 @@
 #!/bin/sh
-
-
-# should ask for all required user input from the beginning then run all the things
-
-collect_password() {
-	printf $1 >&2
-	read -s password
-	printf "Enter the same password again to verify: " >&2
-	read -s password2
-	if [ $password == $password2 ]; then
-		$password
-	else
-		password=$(collect_password $1)
-	fi
-	return $password
-}
-
 lsblk -f
 printf "Name the disk on which to install arch (ex /dev/sda): " >&2
-read -r disk
-printf '%s\n' "$disk"
-
-printf "UEFI or Legacy BIOS?: " >&2
-read -r bootmode
-printf '%s\n' "$bootmode"
-
+read -r DISK
+printf "UEFI or BIOS?: " >&2
+read -r BOOTMODE
 printf "Hostname: " >&2
-read -r hostname
-printf '%s\n' "$hostname"
+read -r HOSTNAME
+printf "Username: " >&2
+read -r USERNAME
 
-diskpasswd=$(collect_password "Enter the password for disk encryption: ")
-
+# Format disk
 # https://stackoverflow.com/questions/12150116/how-to-script-sfdisk-or-parted-for-multiple-partitions
 sfdisk $disk < layout.sfdisk
-
-mkfs.fat -F32 "${disk}1"
-mkfs.swap "${disk}2"
-cryptsetup luksFormat "${disk}3"
-cryptsetup open "${disk}3" unencryptedpartition
+mkfs.fat -F32 "${DISK}1"
+mkfs.swap "${DISK}2"
+cryptsetup luksFormat "${DISK}3"
+cryptsetup open "${DISK}3" unencryptedpartition
 mkfs.ext4 /dev/mapper/unencryptedpartition
 
+# Mount partitions
 mount /dev/mapper/unencryptedpartition /mnt
 mkdir /mnt/boot
-mount "${disk}1" /mnt/boot
-swapon "${disk}2"
+mount "${DISK}1" /mnt/boot
+swapon "${DISK}2"
 
+case $BOOTMODE in
+	UEFI) additionalpackages="efibootmgr";;
+	BIOS) additionalpackages="mtools gptfdisk";;
+	*) "No valid bootmode provided";;
+esac
 
-
-# investigate further how to use syslinux with
-# with the encrypted drive - probably just
-# same kind of operation as with grub
-if [ $bootmode == "UEFI" ]; then
-	efibootmgr syslinux ...
-fi
-pacstrap -K /mnt base linux linux-firmware neovim lvm2
-
+pacstrap -K /mnt base linux linux-firmware lvm2 cryptsetup grub $additionalpackages
 genfstab -U /mnt >> /mnt/etc/fstab
 
-arch-chroot /mnt bash
+# Chroot into the new machine
+arch-chroot /mnt
 ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime
 hwclock --systohc
-
-printf 'LANG=en_US.UTF-8' > /etc/locale.conf
+printf "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
-printf '%s' "$hostname" > /etc/hostname
+printf 'LANG=en_US.UTF-8' > /etc/locale.conf
+printf '%s' "$HOSTNAME" > /etc/hostname
 
+printf '127.0.0.1 localhost' > /etc/hosts
+printf '::1 localhost' >> /etc/hosts
+printf "127.0.1.1 $HOSTNAME.localdomain $HOSTNAME" >> /etc/hosts
 
+printf '%s\n' "Enter a password for the root user:"
+passwd
+useradd -G wheel -m $USERNAME
+printf '%s\n' "Enter a password for $USERNAME:"
+passwd $USERNAME
+
+sed -i 's|^HOOKS=(|HOOKS=(lvm2 encrypt |g' /etc/mkinitcpio.conf
+sed -i "s|^GRUB_CMDLINE_LINUX_DEFAULT.*$|GRUB_CMDLINE_LINUX_DEFAULT=\"loglevel3 quiet cryptdevice=UUID=$(lsblk -o UUID "${DISK}3" | tail -n 1):cryptlvm root=UUID=$(lsblk -o UUID /dev/mapper/unencryptedpartition | tail -n 1) iomem=relaxed\"|g"
+case $BOOTMODE in
+	UEFI) grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=grub;;
+	BIOS) grub-install $DISK;;
+	*) "No valid bootmode provided";;
+esac
+grub-mkconfig -o /boot/grub/grub.cfg
+
+printf '%s\n' "Base system is now installed, exit the chroot and reboot the system (be sure to remove installation media"
